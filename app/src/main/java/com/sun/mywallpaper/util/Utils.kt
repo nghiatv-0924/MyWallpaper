a3f521f1ce43092b1e050cc951ce3dfd20b0c6f5
nghiatv-0924 <tran.van.nghia@sun-asterisk.com>
2019-10-26 19:06:42 +0700

    Create edit photo UI p2

--- app/src/main/java/com/sun/mywallpaper/util/Utils.kt
+++ app/src/main/java/com/sun/mywallpaper/util/Utils.kt
@@ -2,9 +2,16 @@ package com.sun.mywallpaper.util
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.core.app.ActivityCompat
+import androidx.core.content.res.ResourcesCompat
+import com.sun.mywallpaper.R
+import ja.burhanrashid52.photoeditor.PhotoFilter
+import java.io.IOException
 
 object Utils {
 
@@ -19,6 +26,59 @@ object Utils {
         } else {
             true
         }
+
+    fun getColorPicker(resources: Resources, colorCode: Int) =
+        ResourcesCompat.getColor(resources, colorCode, null)
+
+    fun getColorPickerList(resources: Resources): List<Int> = mutableListOf<Int>().apply {
+        add(getColorPicker(resources, R.color.blue_color_picker))
+        add(getColorPicker(resources, R.color.brown_color_picker))
+        add(getColorPicker(resources, R.color.green_color_picker))
+        add(getColorPicker(resources, R.color.orange_color_picker))
+        add(getColorPicker(resources, R.color.red_color_picker))
+        add(getColorPicker(resources, R.color.black_color_picker))
+        add(getColorPicker(resources, R.color.red_orange_color_picker))
+        add(getColorPicker(resources, R.color.sky_blue_color_picker))
+        add(getColorPicker(resources, R.color.violet_color_picker))
+        add(getColorPicker(resources, R.color.white_color_picker))
+        add(getColorPicker(resources, R.color.yellow_color_picker))
+        add(getColorPicker(resources, R.color.yellow_green_color_picker))
+    }
+
+    fun getFilterList(): List<Pair<String, PhotoFilter>> =
+        mutableListOf<Pair<String, PhotoFilter>>().apply {
+            add(Pair(Constants.FILTER_NONE, PhotoFilter.NONE))
+            add(Pair(Constants.FILTER_AUTO_FIX, PhotoFilter.AUTO_FIX))
+            add(Pair(Constants.FILTER_BRIGHTNESS, PhotoFilter.BRIGHTNESS))
+            add(Pair(Constants.FILTER_CONTRAST, PhotoFilter.CONTRAST))
+            add(Pair(Constants.FILTER_DOCUMENTARY, PhotoFilter.DOCUMENTARY))
+            add(Pair(Constants.FILTER_DUE_TONE, PhotoFilter.DUE_TONE))
+            add(Pair(Constants.FILTER_FILL_LIGHT, PhotoFilter.FILL_LIGHT))
+            add(Pair(Constants.FILTER_FISH_EYE, PhotoFilter.FISH_EYE))
+            add(Pair(Constants.FILTER_GRAIN, PhotoFilter.GRAIN))
+            add(Pair(Constants.FILTER_GRAY_SCAL, PhotoFilter.GRAY_SCALE))
+            add(Pair(Constants.FILTER_LOMISH, PhotoFilter.LOMISH))
+            add(Pair(Constants.FILTER_NEGATIVE, PhotoFilter.NEGATIVE))
+            add(Pair(Constants.FILTER_POSTERIZE, PhotoFilter.POSTERIZE))
+            add(Pair(Constants.FILTER_SATURATE, PhotoFilter.SATURATE))
+            add(Pair(Constants.FILTER_SEPIA, PhotoFilter.SEPIA))
+            add(Pair(Constants.FILTER_SHARPEN, PhotoFilter.SHARPEN))
+            add(Pair(Constants.FILTER_TEMPERATURE, PhotoFilter.TEMPERATURE))
+            add(Pair(Constants.FILTER_TINT, PhotoFilter.TINT))
+            add(Pair(Constants.FILTER_VIGNETTE, PhotoFilter.VIGNETTE))
+            add(Pair(Constants.FILTER_CROSS_PROCESS, PhotoFilter.CROSS_PROCESS))
+            add(Pair(Constants.FILTER_FLIP_HORIZONTAL, PhotoFilter.FLIP_HORIZONTAL))
+            add(Pair(Constants.FILTER_FLIP_VERTICAL, PhotoFilter.FLIP_VERTICAL))
+            add(Pair(Constants.FILTER_ROTATE, PhotoFilter.ROTATE))
+        }
+
+    fun getBitmapFromAsset(context: Context, name: String) = try {
+        val inputStream = context.assets.open(name)
+        BitmapFactory.decodeStream(inputStream)
+    } catch (e: IOException) {
+        e.printStackTrace()
+        null
+    }
 
     private fun checkSelfPermission(activity: Activity) = ActivityCompat.checkSelfPermission(
         activity,
